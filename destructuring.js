5c7e944a06c0d479177c2f02a734bbef6f1c71c7
SandeepReddy Chinthakunta <sandeeprddy23@gmail.com>
2021-08-02 20:20:51 +0530

    destructuring added

---
+++ destructuring.js
@@ -0,0 +1,8 @@
+let obj = {
+  name: "Sandeep",
+  city : "Medak",
+  pin : 502334
+}
+let{name,pin} = obj;
+console.log(name);
+console.log(pin);
--- spread.js
+++ spread.js
@@ -4,3 +4,15 @@ let res = [...arr.slice(0,2),3,...arr.slice(2)];
 console.log(res);
 let spreadArr = [...arr,...b];
 console.log(spreadArr);
+let obj1 = {
+    a:1,
+    b:2
+}
+let obj2 = {
+    c:3,
+    d:4
+}
+let obj3 = {...obj1,...obj2};
+let obj4 = {...obj1,...obj2,...obj1,...obj2};
+console.log(obj3);
+console.log(obj4);
