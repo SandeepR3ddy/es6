1ca5f86f3864d73b69e3b2e29b8359885705a51d
SandeepReddy Chinthakunta <sandeeprddy23@gmail.com>
2021-08-02 20:49:07 +0530

    syntax of arrow function

---
+++ arrowFunctions.js
@@ -0,0 +1,24 @@
+// normal function
+function f(x,y)
+{
+    return x*y;
+}
+
+//-------------
+  
+//arrow function
+ 
+let fun = (x,y) => {
+   return x*y;
+}
+
+//single argument arrow function
+
+
+let fun2 = x => {
+    return 2*x;
+}
+
+//if have a single statement inside the function body and that too return statement
+
+let f3 = x => 2*x;
